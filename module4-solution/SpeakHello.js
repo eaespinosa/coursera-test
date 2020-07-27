0aa8c29ae8762b373e38ba503342fb24339d3f0d
eaespinosa <65977866+eaespinosa@users.noreply.github.com>
2020-07-26 20:01:19 -0500

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,6 +1,10 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
+(function (window) {
+  var helloSpeaker = {};
+  helloSpeaker.speak = name; 
+
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -8,15 +12,18 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
-
+  var speakWord = "Hello";
+  helloSpeaker.speak = function () {
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
+    
+    console.log(speakWord + " " + hellospeaker.speak);
 }
 
+window.helloSpeaker = helloSpeaker;
+
+})(window);
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
